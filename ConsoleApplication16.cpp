0ad035e58c8476a68679406abc05bd0f2a980601
Nizhegorodov Aleksei <nizhegorodov1619@gmail.com>
2023-12-02 08:37:13 +0300

    тест ветки

--- ConsoleApplication16.cpp
+++ ConsoleApplication16.cpp
@@ -1,31 +1,37 @@
-﻿#include <iostream> 
+﻿
+#include <iostream> 
 #include <fstream> 
 #include <string.h> 
 #include <stdio.h> 
 #include <vector> 
 using namespace std;
-
-struct pipe {
+/// @brief struct of pipe one 
+struct pipe_data {
     double L;
     double V;
-    int n;    /// Число разбиений трубопровода
-    int T;    ///T - период моделирования  
+    int num_segmetations;    //Число разбиений трубопровода
+   
+    
+};
+
+struct calc_data {
+    int T;    //T - период моделирования  
     double ro_n;  // Начальная плотность нефти в трубе 
     double sera_n; // Начальное содержание серы в нефти
-    
 };
 
 int main()
 {
-    pipe one = { 200, 50, 9, 5, 850, 0.1 };
+    pipe_data pipe_info = { 200, 50, 9 };
+    calc_data calc_info = {5, 850, 0.1 };
 
 
     /// Корректный вывод руского текста
     setlocale(LC_ALL, "Russian");
 
-    double dx = one.L / (one.n - 1);
-    double dt = dx / one.V;     //условие Куранта 
-    int N = (one.T / dt);  // Число слоев
+    double dx = pipe_info.L / (pipe_info.num_segmetations - 1);
+    double dt = dx / pipe_info.V;     //условие Куранта 
+    int num_layers = (calc_info.T / dt);  // Число слоев
     
   
 
@@ -39,29 +45,29 @@ int main()
     ///// Записываем заголовки колонок
     Overview << "Время,Координата,Плотность,Содержание серы" << std::endl;
 
-    vector<double> ro_in(N); // Вектор плотностей нефти входных партий
+    vector<double> ro_in(num_layers); // Вектор плотностей нефти входных партий
     ro_in = { 850, 870, 860, 860, 860,860, 860, 860, 860, 860 };
 
-    vector <double> ro_0(one.n); // Начальный слой по плотности
-    vector<double> ro_1(one.n);   // Следующий слой по плотности
+    vector <double> ro_0(pipe_info.num_segmetations); // Начальный слой по плотности
+    vector<double> ro_1(pipe_info.num_segmetations);   // Следующий слой по плотности
 
 
-    vector<double> sera_in(N);//// Начальный слой по сере
+    vector<double> sera_in(num_layers);//// Начальный слой по сере
     sera_in = { 0.01, 0.02, 0.03, 0.04, 0.05,0.06, 0.07, 0.08, 0.09, 0.01 };
 
-    vector <double> sera_0(one.n); // Начальный слой по сере
-    vector<double> sera_1(one.n); // Следующий слой по сере
+    vector <double> sera_0(pipe_info.num_segmetations); // Начальный слой по сере
+    vector<double> sera_1(pipe_info.num_segmetations); // Следующий слой по сере
      
     // Предполагаем, что в начальный момент времени всю трубу заполняют  нефть с плотностью ro_n
-    for (int k = 0; k < one.n; k++) {
-        ro_0[k] = one.ro_n;
-        sera_0[k] = one.sera_n;
+    for (int k = 0; k < pipe_info.num_segmetations; k++) {
+        ro_0[k] = calc_info.ro_n;
+        sera_0[k] =  calc_info.sera_n;
         cout << ro_0[k] << endl;
         cout << sera_0[k] << endl;
-        Overview << 0 << "," << dx * k << "," << one.ro_n << "," << one.sera_n << endl;
+        Overview << 0 << "," << dx * k << "," << calc_info.ro_n << "," << calc_info.sera_n << endl;
     }
     // Рассчёт слоев 
-    for (int j = 0; j < N; j++) {
+    for (int j = 0; j < num_layers; j++) {
         //cout << "Слой " << j + 1 << endl;
         ro_1[0] = ro_in[j];
         sera_1[0] = sera_in[j];
@@ -71,7 +77,7 @@ int main()
         Overview << dt * (j + 1) << "," << 0 << "," << ro_1[0] << "," << sera_1[0] << endl;
 
         // Слудующий слой
-        for (int i = 1; i < one.n; i++) {
+        for (int i = 1; i < pipe_info.num_segmetations; i++) {
             ro_1[i] = ro_0[i - 1];
             sera_1[i] = sera_0[i - 1];
             Overview << dt * (j + 1) << "," << dx * i << "," << ro_1[i] << "," << sera_1[i] <<  endl;
