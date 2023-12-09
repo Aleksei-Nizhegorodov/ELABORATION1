acf3656463f80307c33d299c707d15f45804e958
Nizhegorodov Aleksei <nizhegorodov1619@gmail.com>
2023-12-09 15:55:40 +0300

    добавил конструктор

--- ConsoleApplication16.cpp
+++ ConsoleApplication16.cpp
@@ -12,6 +12,15 @@ struct pipe_data {
     double speed; //Средняя скрость движения партий
     int num_segmetations;    //Число разбиений трубопровода
      
+    pipe_data() {
+        L = 200;
+        speed = 50;
+        num_segmetations = 9;
+
+        cout << "Длина трубопровода:                        " << L << endl;
+        cout << "Значение средней скрости движения партий:  " << speed << endl;
+        cout << "Число разбеений трубопровода:              " << num_segmetations << endl;
+    }
 };
 
 /// @brief Структура данных для расчета 
@@ -21,38 +30,26 @@ struct calc_data {
     double sera_n; // Начальное содержание серы в нефти
     int num_layers; //Кол-во слоев
     double dt, dx; // Шаги по времени, координате
+
+    calc_data(pipe_data& iniPipe){
+    
+        T = 5;
+        ro_n = 850;
+        sera_n = 0.1;
+
+        dx = iniPipe.L / (iniPipe.num_segmetations - 1);
+        dt = dx / iniPipe.speed;
+        num_layers = (T / dt);
+
+        cout << "Время моделирования:                      " << T << endl;
+        cout << "Значение плотности в начале:              " << ro_n << endl;
+        cout << "Значение серы в начале:                   " << sera_n << endl;
+        cout << "Число слоев:                              " << num_layers << endl;
+        cout << "Шаг по координате:                        " << ro_n << endl;
+        cout << "Шаг по времени:                           " << sera_n << endl;
+    }
 };
 
-void Pipe(pipe_data& iniPipe) {
-    iniPipe.L = 200;
-    iniPipe.speed = 50;
-    iniPipe.num_segmetations = 9;
-
-    cout << "Длина трубопровода:                        " << iniPipe.L << endl;
-    cout << "Значение средней скрости движения партий:  " << iniPipe.speed << endl;
-    cout << "Число разбеений трубопровода:              " << iniPipe.num_segmetations << endl;
-
-};
-
-void Calc(calc_data& iniCalc, pipe_data& iniPipe) {
-    iniCalc.T = 5;
-    iniCalc.ro_n = 850;
-    iniCalc.sera_n = 0.1;
-
-    iniCalc.num_layers = (iniCalc.T / iniCalc.dt);
-    iniCalc.dx = iniPipe.L / (iniPipe.num_segmetations - 1);
-    iniCalc.dt = iniCalc.dx / iniPipe.speed;
-
-
-    cout << "Время моделирования:                      " << iniCalc.T << endl;
-    cout << "Значение плотности в начале:              " << iniCalc.ro_n << endl;
-    cout << "Значение серы в начале:                   " << iniCalc.sera_n << endl;
-    cout << "Число слоев:                              " << iniCalc.num_layers << endl;
-    cout << "Шаг по координате:                        " << iniCalc.ro_n << endl;
-    cout << "Шаг по времени:                           " << iniCalc.sera_n << endl;
-
-
-};
 
 int main()
 {
@@ -69,11 +66,7 @@ int main()
     Overview << "Время,Координата,Плотность,Содержание серы" << std::endl; // Записываем заголовки колонок
     
     pipe_data iniPipe; //объявление переменной iniPipe (хранение данных о трубопроводе) типа pipe_data
-    calc_data iniCalc;  //объявление переменной iniCalc типа calc_data
-
-    Pipe(iniPipe); // это вызов функции Pipe с аргументом iniPipe. Функция Pipe принимает ссылку на структуру pipe_data и использует эту ссылку для изменения значений полей структуры
-    Calc(iniCalc, iniPipe); //это вызов функции Calc с аргументом iniCalc и iniPipe.Функция Calc принимает ссылку на структуру calc_data и использует эту ссылку для изменения значений полей структуры
-
+    calc_data iniCalc(iniPipe);  //объявление переменной iniCalc типа calc_data
 
     vector<double> ro_in(iniCalc.num_layers); // Вектор плотностей нефти входных партий
     ro_in = { 850, 870, 860, 860, 860,860, 860, 860, 860, 860 };
@@ -89,13 +82,16 @@ int main()
     vector<double> sera_1(iniPipe.num_segmetations); // Следующий слой по сере
 
     // Предполагаем, что в начальный момент времени всю трубу заполняют  нефть с плотностью ro_n
+    //тут можно сделать отедльную функцию с 85 строчки до конца
     for (int k = 0; k < iniPipe.num_segmetations; k++) {
         ro_0[k] = iniCalc.ro_n;
         sera_0[k] = iniCalc.sera_n;
+
         cout << ro_0[k] << endl;
         cout << sera_0[k] << endl;
+
         Overview << 0 << "," << iniCalc.dx * k << "," << iniCalc.ro_n << "," << iniCalc.sera_n << endl;
-    }
+    };
     // Рассчёт слоев 
     for (int j = 0; j < iniCalc.num_layers; j++) {
         //cout << "Слой " << j + 1 << endl;
@@ -104,7 +100,6 @@ int main()
         cout << ro_1[0] << endl;
         cout << sera_1[0] << endl;
 
-
         Overview << iniCalc.dt * (j + 1) << "," << 0 << "," << ro_1[0] << "," << sera_1[0] << endl; // Запись  в файл
 
         // Слудующий слой
@@ -112,8 +107,10 @@ int main()
             ro_1[i] = ro_0[i - 1];
             sera_1[i] = sera_0[i - 1];
             Overview << iniCalc.dt * (j + 1) << "," << iniCalc.dx * i << "," << ro_1[i] << "," << sera_1[i] << endl;
+        
         }
         ro_0 = ro_1;
         sera_0 = sera_1;
     };
+    Overview.close();
 };
