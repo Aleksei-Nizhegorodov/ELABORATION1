6b8350a8aa9945b4a9c549c55f202696de5a4af1
Nizhegorodov Aleksei <nizhegorodov1619@gmail.com>
2024-01-05 21:18:10 +0300

    Исправлены ошибки в процессе PR

--- ConsoleApplication16.cpp
+++ ConsoleApplication16.cpp
@@ -1,5 +1,4 @@
-﻿
-#include <iostream> 
+﻿#include <iostream> 
 #include <fstream> 
 #include <string.h> 
 #include <stdio.h> 
@@ -52,36 +51,40 @@ struct calc_data {
         cout << "Шаг по времени:                           " << sera_n << endl;*/
     }
 };
+
 /// @brief отдельная функция для вывода значений расчета
-/// @param iniCalc 
-/// @param buffer 
-/// @param i 
-void output_fun (calc_data iniCalc, ring_buffer_t<vector<vector<double>>>& buffer, int i) {
-    vector<vector<double>>& current_layer = buffer.current();
+/// @param iniCalc объявление переменной iniCalc (Данные о параметрах в трубопроводе)
+/// @param buffer объявление переменной buffer
+void output_fun (calc_data iniCalc, vector<vector<double>>& current_layer, int i) {
+    
     if (i == 0) {
         ofstream outFile("output.csv");
         outFile << "время,координиата,плотность, сера" << "\n";
         // Записать значения текущего слоя в файл
-        for (size_t j = 0; j < current_layer[0].size(); j++) {
-            outFile << i * iniCalc.dt << "," << j * iniCalc.dx << "," << current_layer[0][j] << "," << current_layer[1][j] << "\n";
+        for (size_t j = 0; j < current_layer.size(); j++) {
+            for (size_t k = 0; k < current_layer[j].size(); k++) {
+                outFile << i * iniCalc.dt << "," << j * iniCalc.dx << "," << current_layer[j][k] << "\n";
+            }
         }
         outFile.close();
     }
     else {
         ofstream outFile("output.csv", ios::app);
         // Записать значения текущего слоя в файл
-        for (size_t j = 0; j < current_layer[0].size(); j++) {
-            outFile << i * iniCalc.dt << "," << j * iniCalc.dx << "," << current_layer[0][j] << "," << current_layer[1][j] << "\n";
+        for (size_t j = 0; j < current_layer.size(); j++) {
+            for (size_t k = 0; k < current_layer[j].size(); k++) {
+                outFile << i * iniCalc.dt << "," << j * iniCalc.dx << "," << current_layer[j][k] << "\n";
+            }
         }
         outFile.close();
     }
 }
 /// @brief отдельная функция для метода характеристик
-/// @param iniPipe 
-/// @param argument 
-/// @param current_layer 
-/// @param previous_layer 
-void calculation(pipe_data iniPipe, double argument, vector<double>& current_layer, vector<double>& previous_layer) //Данные о параметрах трубопровода; нач.значения; вектор тек.слоя; вектор пред.слоя
+/// @param iniPipe объявление переменной iniPipe (Данные о параметрах трубопровода)
+/// @param argument  нач.значения
+/// @param current_layer вектор текущий слой 
+/// @param previous_layer вектор предыущий слой
+void calculation(pipe_data iniPipe, double argument, vector<double>& current_layer, vector<double>& previous_layer) 
 {
     // Метод характеристик
     for (size_t i = 1; i < current_layer.size(); i++)
@@ -109,49 +112,15 @@ int main()
     vector<double> double_array(iniPipe.num_segmetations); //Создание массива
     ring_buffer_t<vector<vector<double>>> buffer(2, {double_array, double_array});  
     
-    //Расчет методом bufer
+    //Расчет методом характеристик с использованием buffer
    
     for (size_t i = 0; i < iniCalc.num_layers; i++) {
-        calculation(iniPipe, ro_in[i], buffer.current()[0], buffer.previous()[0]);
-        calculation(iniPipe, sera_in[i], buffer.current()[1], buffer.previous()[1]);
+        for (size_t j = 0; j < buffer.current().size(); j++) {
+            calculation(iniPipe, ro_in[i], buffer.current()[j], buffer.previous()[j]);
+            calculation(iniPipe, sera_in[i], buffer.current()[j], buffer.previous()[j]);
+        }
         buffer.advance(1);
-        output_fun(iniPipe, buffer, i);
-    }        
+        output_fun(iniCalc, buffer.current(), i);
+    }
     
-    // кол-во итераций на 114 стр должно соотв числу граничных условий плоности и серы 
-    // 
-
-
-    //// Предполагаем, что в начальный момент времени всю трубу заполняют  нефть с плотностью ro_n
-    ////тут можно сделать отедльную функцию с 85 строчки до конца
-    //for (int k = 0; k < iniPipe.num_segmetations; k++) {
-    //    ro_0[k] = iniCalc.ro_n;
-    //    sera_0[k] = iniCalc.sera_n;
-
-    //    cout << ro_0[k] << endl;
-    //    cout << sera_0[k] << endl;
-
-    //    Overview << 0 << "," << iniCalc.dx * k << "," << iniCalc.ro_n << "," << iniCalc.sera_n << endl;
-    //};
-    //// Рассчёт слоев 
-    //for (int j = 0; j < iniCalc.num_layers; j++) {
-    //    //cout << "Слой " << j + 1 << endl;
-    //    ro_1[0] = ro_in[j];
-    //    sera_1[0] = sera_in[j];
-    //    cout << ro_1[0] << endl;
-    //    cout << sera_1[0] << endl;
-
-    //    Overview << iniCalc.dt * (j + 1) << "," << 0 << "," << ro_1[0] << "," << sera_1[0] << endl; // Запись  в файл
-
-    //    // Слудующий слой
-    //    for (int i = 1; i < iniPipe.num_segmetations; i++) {
-    //        ro_1[i] = ro_0[i - 1];
-    //        sera_1[i] = sera_0[i - 1];
-    //        Overview << iniCalc.dt * (j + 1) << "," << iniCalc.dx * i << "," << ro_1[i] << "," << sera_1[i] << endl;
-    //    
-    //    }
-    //    ro_0 = ro_1;
-    //    sera_0 = sera_1;
-    //};
-    //Overview.close();
 };
